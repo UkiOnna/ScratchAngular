b687db54cd04250cbb2cbec77956eb8796b81a7d
Evgeniy Pavlov <djoniqaz@gmail.com>
2019-11-20 17:21:23 +0600

    chore(personal-statistic) инициировал

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { SignInComponent } from './components/sign-in/sign-in.component';
 import { MainPageComponent } from './components/main-page/main-page.component';
 import { CookieService } from 'ngx-cookie-service';
 import { DialogAddUser } from './components/navbar/navbar.component';
+import { PersonalStatisticComponent } from './components/personal-statistic/personal-statistic.component';
 
 
 const routes: Routes = [
   { path: 'login', component: SignInComponent },
-  { path: 'main-page', component: MainPageComponent}
+  { path: 'main-page', component: MainPageComponent},
+  { path: 'personal-statistic', component: PersonalStatisticComponent}
 ];
 
 @NgModule({
