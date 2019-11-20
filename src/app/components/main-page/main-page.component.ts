b711d9c771bd7028bfd9d0924a2633adb648aa50
Evgeniy Pavlov <djoniqaz@gmail.com>
2019-11-20 16:28:48 +0600

    chore(main-page) исправил добавление intervalCell

--- src/app/components/main-page/main-page.component.ts
+++ src/app/components/main-page/main-page.component.ts
@@ -46,6 +46,7 @@ export class MainPageComponent implements OnInit {
                 hour: i,
                 isWork: isWork
               }
+              intervalCells.push(intervalCell);
             }
 
             let element: TaskViewModel = {
