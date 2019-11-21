464a4376a58e4c614d2c526644d80081c8b39cd3
UkiOnna <sikirovtemirlan@mail.ru>
2019-11-21 14:49:20 +0600

    chore(dialogAddUser)начал писать логику создания юзера в бд

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -3,6 +3,10 @@ import { CookieService } from 'ngx-cookie-service';
 import { UsersService } from 'src/app/services/users.service';
 import { UserDto } from 'src/app/models/user.model';
 import { MatDialogRef, MatDialog, MAT_DIALOG_DATA } from '@angular/material';
+import { DepartmentsService } from 'src/app/services/departments.service';
+import { SubdivisionsService } from 'src/app/services/subdivisions.service';
+import { SubdivisionDto } from 'src/app/models/subdivision.model';
+import { DepartmentDto } from 'src/app/models/department.model';
 
 @Component({
   selector: 'app-navbar',
@@ -14,7 +18,7 @@ export class NavbarComponent implements OnInit {
   public isLogin: boolean = false;
   public isDepartment: boolean = false;
   public isDivision: boolean = false;
-  constructor(private cookieService: CookieService, private userSerivce: UsersService,public dialog: MatDialog) { }
+  constructor(private cookieService: CookieService, private userSerivce: UsersService, public dialog: MatDialog) { }
 
   ngOnInit() {
     let cookieValue = this.cookieService.get("token");
@@ -28,7 +32,7 @@ export class NavbarComponent implements OnInit {
             this.isDivision = true;
             break;
           case 3:
-            this.isDepartment=true;
+            this.isDepartment = true;
         }
       });
       this.isLogin = true;
@@ -47,15 +51,40 @@ export class NavbarComponent implements OnInit {
       console.log('The dialog was closed');
     });
 
-}
+  }
 }
 
 @Component({
   selector: 'dialogAddUser',
   templateUrl: 'dialogAddUser.html',
 })
-export class DialogAddUser {
+export class DialogAddUser implements OnInit {
+  public subDivisions:SubdivisionDto[]=[];
+  public departaments:DepartmentDto[]=[];
+  public name:string;
+  public middlename:string;
+  public lastname:string;
+  user:UserDto;
 
-  constructor( public dialogRef: MatDialogRef<DialogAddUser>){}
+  constructor(public dialogRef: MatDialogRef<DialogAddUser>, private departamentService: DepartmentsService, private subdivisionService: SubdivisionsService,private userService:UsersService) { }
+
+  ngOnInit() {
+    this.subdivisionService.getSubdivisions().subscribe(subdivisions => {
+      this.subDivisions=subdivisions;
+    });
+  }
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+  onYesClick(): void {
+    this.user.firstName=this.name;
+    this.user.lastName=this.lastname;
+    this.user.middleName=this.middlename;
+    //добавить id департамента и подразделения
+    this.userService.updateUser(this.user);
+    this.dialogRef.close();
+  }
+  
 
 }
