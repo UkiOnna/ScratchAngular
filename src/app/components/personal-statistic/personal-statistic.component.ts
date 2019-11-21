42496a4e7710276fd7d64f97ded62a14a8acaefd
Evgeniy Pavlov <djoniqaz@gmail.com>
2019-11-21 17:56:07 +0600

    feat(models) добавил view для personal-statistic

--- src/app/components/personal-statistic/personal-statistic.component.ts
+++ src/app/components/personal-statistic/personal-statistic.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { TaskPersonStatisticViewModel } from 'src/app/models/taskPersonStatisticView.model';
+import { TasksService } from 'src/app/services/tasks.service';
+import { ProjectsService } from 'src/app/services/projects.service';
+import { IntervalsService } from 'src/app/services/intervals.service';
+import { UserDto } from 'src/app/models/user.model';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-personal-statistic',
@@ -7,9 +13,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PersonalStatisticComponent implements OnInit {
 
-  constructor() { }
+  user: UserDto;
+  tasks: TaskPersonStatisticViewModel[] = [];
+  startDate = new FormControl(new Date());
+  endDate = new FormControl(new Date());
+
+  constructor(private tasksService: TasksService,
+    private intervalsService: IntervalsService,
+    private projectsService: ProjectsService) { }
 
   ngOnInit() {
+    let indexDate: Date = this.startDate.value;
+    while (indexDate <= this.endDate.value) {
+    }
   }
 
 }
---
+++ src/app/models/taskPersonStatisticView.model.ts
@@ -0,0 +1,8 @@
+export class TaskPersonStatisticViewModel {
+  id: number;
+  projectName: string;
+  taskName: string;
+  todayWorkTime: number;
+  allWorkTIme: number;
+  taskStatus: string;
+}
