98d8a8bd960dd81a6ddbddb54e50cb521c70ac9d
UkiOnna <sikirovtemirlan@mail.ru>
2019-11-23 15:37:35 +0600

    chore(dialog-add-user)дописал логику создания юзера изменил расположение элементов по центру

--- src/app/components/dialogs/dialog-add-user/dialog-add-user.component.ts
+++ src/app/components/dialogs/dialog-add-user/dialog-add-user.component.ts
@@ -1,11 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { SubdivisionDto } from 'src/app/models/subdivision.model';
-import { DepartmentDto } from 'src/app/models/department.model';
 import { UserDto } from 'src/app/models/user.model';
 import { MatDialogRef } from '@angular/material';
 import { DepartmentsService } from 'src/app/services/departments.service';
 import { SubdivisionsService } from 'src/app/services/subdivisions.service';
 import { UsersService } from 'src/app/services/users.service';
+import { SubdivisionDto } from 'src/app/models/subdivision.model';
+import { DepartmentDto } from 'src/app/models/department.model';
 
 @Component({
   selector: 'dialogAddUser',
@@ -13,32 +13,50 @@ import { UsersService } from 'src/app/services/users.service';
   styleUrls: ['./dialog-add-user.component.scss']
 })
 export class DialogAddUser implements OnInit {
-  public subDivisions:SubdivisionDto[]=[];
-  public departaments:DepartmentDto[]=[];
-  public name:string;
-  public middlename:string;
-  public lastname:string;
-  user:UserDto;
+  public subDivisions: SubdivisionDto[] = [];
+  public departaments: DepartmentDto[] = [];
+  public selectedDepartamentId: number;
+  public selectedSubdivisionId: number;
+  public name: string;
+  public middlename: string;
+  public lastname: string;
+  user: UserDto;
+  isError:boolean=false;
 
-  constructor(public dialogRef: MatDialogRef<DialogAddUser>, private departamentService: DepartmentsService, private subdivisionService: SubdivisionsService,private userService:UsersService) { }
+  constructor(public dialogRef: MatDialogRef<DialogAddUser>, private departamentService: DepartmentsService, private subdivisionService: SubdivisionsService, private userService: UsersService) { }
 
   ngOnInit() {
     this.subdivisionService.getSubdivisions().subscribe(subdivisions => {
-      this.subDivisions=subdivisions;
+      this.subDivisions = subdivisions;
     });
   }
 
+  subDivisionChanged(value): void {
+    this.departamentService.getSubdivisionDepartments(value).subscribe(departaments => {
+      this.departaments = departaments;
+    });
+    this.selectedSubdivisionId = value;
+  }
+
+  departamentChanged(value): void {
+    this.selectedDepartamentId = value;
+  }
   onNoClick(): void {
     this.dialogRef.close();
   }
   onYesClick(): void {
-    this.user.firstName=this.name;
-    this.user.lastName=this.lastname;
-    this.user.middleName=this.middlename;
-    //добавить id департамента и подразделения
-    this.userService.updateUser(this.user);
-    this.dialogRef.close();
+    if (this.name && this.lastname && this.middlename && this.selectedDepartamentId) {
+      this.user.firstName = this.name;
+      this.user.lastName = this.lastname;
+      this.user.middleName = this.middlename;
+      this.user.departmentId = this.selectedDepartamentId;
+      this.userService.updateUser(this.user);
+      this.dialogRef.close();
+    }
+    else{
+      this.isError=true;
+    }
   }
-  
+
 
 }
