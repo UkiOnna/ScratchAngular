6fa460c47d8904fab9d200e1793224baf4670bac
UkiOnna <sikirovtemirlan@mail.ru>
2019-11-25 13:18:22 +0600

    chore(dialog-add-task)Поправил добавление дедлайна

--- src/app/components/dialogs/dialog-add-task/dialog-add-task.component.ts
+++ src/app/components/dialogs/dialog-add-task/dialog-add-task.component.ts
@@ -4,6 +4,7 @@ import { ProjectsService } from 'src/app/services/projects.service';
 import { ProjectDto } from 'src/app/models/project.model';
 import { TaskDto } from 'src/app/models/task.model';
 import { TasksService } from 'src/app/services/tasks.service';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-dialog-add-task',
@@ -14,43 +15,40 @@ export class DialogAddTaskComponent implements OnInit {
 
   public name: string;
   public description: string;
-  public deadline: Date;
+  public deadline = new FormControl(new Date());
   userId: number;
   public projects: ProjectDto[] = [];
-  selectedProjectId:number;
-  task:TaskDto;
-  isError:boolean=false;
-  constructor(public dialogRef: MatDialogRef<DialogAddTaskComponent>,@Inject(MAT_DIALOG_DATA) public data: any, private projectService: ProjectsService,private taskService:TasksService) { }
+  selectedProjectId: number;
+  task: TaskDto;
+  isError: boolean = false;
+  constructor(public dialogRef: MatDialogRef<DialogAddTaskComponent>, @Inject(MAT_DIALOG_DATA) public data: any, private projectService: ProjectsService, private taskService: TasksService) { }
 
   ngOnInit() {
     this.userId = this.data["userId"];
+
     this.projectService.getProjects().subscribe(projects => {
       this.projects = projects;
     });
   }
 
-  cahngeDate(value): void {
-    this.deadline = value;
-  }
-
-  projectChanged(value):void{
-    this.selectedProjectId=value;
+  projectChanged(value): void {
+    this.selectedProjectId = value;
   }
 
   onNoClick(): void {
     this.dialogRef.close();
   }
   onYesClick(): void {
-    if (this.name && this.deadline && this.description && this.selectedProjectId) {
-      this.task.description=this.description;
-      this.task.deadline=this.deadline;
-      this.task.title=this.name;
-      this.task.creatorId=this.userId;
+    if (this.name && this.deadline && this.description) {
+      this.task.description = this.description;
+      this.task.deadline = this.deadline.value;
+      this.task.title = this.name;
+      this.task.creatorId = this.userId;
       this.taskService.addTask(this.task);
       this.dialogRef.close();
     }
-    else{
-      this.isError=true;
+    else {
+      this.isError = true;
     }
   }
 }
