cdfcf485039335af802c35593be4098d9f745124
Evgeniy Pavlov <djoniqaz@gmail.com>
2019-11-25 17:39:36 +0600

    feat(models) добавил userWorkOnTheTask

---
+++ src/app/models/Dtos/userWorkOnTheTask.model.ts
@@ -0,0 +1,10 @@
+export class TaskDto {
+  id: number;
+  creatorId: number;
+  title: string;
+  description: string;
+  comment: string;
+  deadline: Date;
+  creatorId: number;
+  projectId: number;
+}
