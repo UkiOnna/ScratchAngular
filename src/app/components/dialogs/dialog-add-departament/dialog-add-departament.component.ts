d6e06e4b608ce7966a1d3e5c994b1d9a4b2a639b
UkiOnna <sikirovtemirlan@mail.ru>
2019-11-27 16:30:09 +0600

    feat(dialog-department)Создал логику для редактирования департаментов

--- src/app/components/dialogs/dialog-add-departament/dialog-add-departament.component.ts
+++ src/app/components/dialogs/dialog-add-departament/dialog-add-departament.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
 import { SubdivisionDto } from 'src/app/models/Dtos/subdivision.model';
 import { DepartmentDto } from 'src/app/models/Dtos/department.model';
-import { MatDialogRef } from '@angular/material';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 import { DepartmentsService } from 'src/app/services/departments.service';
 import { SubdivisionsService } from 'src/app/services/subdivisions.service';
 
@@ -14,13 +14,21 @@ export class DialogAddDepartamentComponent implements OnInit {
 
   public name: string;
   public subDivisions: SubdivisionDto[] = [];
+  public departaments: DepartmentDto[] = [];
   selectedSubdivisionId: number;
+  selectedDepartamentId: number = null;
   departament: DepartmentDto;
   isError: boolean = false;
+  isEdit: boolean = false;
+  isDepartmentChoosed: boolean = true;
 
-  constructor(public dialogRef: MatDialogRef<DialogAddDepartamentComponent>, private departamentService: DepartmentsService, private subdivisionService: SubdivisionsService) { }
+  constructor(public dialogRef: MatDialogRef<DialogAddDepartamentComponent>, @Inject(MAT_DIALOG_DATA) public data: any, private departamentService: DepartmentsService, private subdivisionService: SubdivisionsService) { }
 
   ngOnInit() {
+    this.isEdit = this.data["isEdit"];
+    if (this.isEdit) {
+      this.isDepartmentChoosed = false;
+    }
     this.subdivisionService.getSubdivisions().subscribe(subs => {
       this.subDivisions = subs;
     });
@@ -28,6 +36,19 @@ export class DialogAddDepartamentComponent implements OnInit {
 
   subDivisionChanged(value): void {
     this.selectedSubdivisionId = value;
+    if (this.isEdit) {
+      this.departamentService.getSubdivisionDepartments(value).subscribe(deps => {
+        this.departaments = deps;
+      });
+    }
+  }
+
+  departamentChanged(value): void {
+    this.departamentService.getDepartment(value).subscribe(dep => {
+      this.name = dep.name;
+    });
+    this.isDepartmentChoosed = true;
+    this.selectedDepartamentId = value;
   }
 
   onNoClick(): void {
@@ -38,14 +59,18 @@ export class DialogAddDepartamentComponent implements OnInit {
       this.departament = {
         name: this.name,
         subdivisionId: this.selectedSubdivisionId,
-        id: null
+        id: this.selectedDepartamentId
       };
+      if (this.selectedDepartamentId) {
+        this.departamentService.updateDepartment(this.departament);
+        this.dialogRef.close();
+        return;
+      }
       this.departamentService.addDepartment(this.departament);
       this.dialogRef.close();
+      return;
     }
-    else {
-      this.isError = true;
-    }
+    this.isError = true;
   }
 
 }
