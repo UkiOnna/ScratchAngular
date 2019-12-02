2d8c3f18e9f1b02ee3ecadd849a07b6982e27a59
JekaLion <djoni-01@mail.ru>
2019-12-02 19:46:55 +0600

    feat(departments) реализовал

--- src/app/components/departments/departments.component.ts
+++ src/app/components/departments/departments.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { SubdivisionDto } from 'src/app/models/Dtos/subdivision.model';
+import { DepartmentDto } from 'src/app/models/Dtos/department.model';
+import { DepartmentsService } from 'src/app/services/departments.service';
+import { MatTableDataSource } from '@angular/material';
 
 @Component({
   selector: 'app-departments',
@@ -7,9 +11,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DepartmentsComponent implements OnInit {
 
-  constructor() { }
+  @Input() subdivision: SubdivisionDto;
+
+  displayedColumns: string[] = ['id', 'name'];
+  departments = new MatTableDataSource([]);
+  searchValue = '';
+
+  constructor(private departmentsService: DepartmentsService) { }
 
   ngOnInit() {
+    this.departmentsService.getSubdivisionDepartments(this.subdivision.id).subscribe(result => {
+      this.departments.data = result;
+    });
   }
 
+  applyFilter(filterValue: string) {
+    this.departments.filter = filterValue.trim().toLowerCase();
+  }
 }
