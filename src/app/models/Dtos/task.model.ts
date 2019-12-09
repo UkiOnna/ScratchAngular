e444da9a1a3f203346d4882bec8d047d1df0b9f6
UkiOnna <ukionna08@gmail.com>
2019-12-09 21:22:40 +0600

    chore(dto.task.model)Изменил названия свойств для круд общения

--- src/app/models/Dtos/task.model.ts
+++ src/app/models/Dtos/task.model.ts
@@ -2,8 +2,8 @@ export class TaskDto {
   id: number;
   title: string;
   description: string;
-  deadline: Date;
-  creator_id: number;
-  executor_id:number;
-  project_id: number;
+  deadline: string;
+  creatorId: number;
+  executorId:number;
+  projectId: number;
 }
